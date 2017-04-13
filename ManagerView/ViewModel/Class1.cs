f3e72e563df3cd3720a0f05b3e06ad8ef5d02378
Daria <lysy_johnny@bk.ru>
2017-04-13 11:15:53 +0700

    3

--- ManagerView/ViewModel/Class1.cs
+++ ManagerView/ViewModel/Class1.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Model;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using GalaSoft.MvvmLight;
+using GalaSoft.MvvmLight.Command;
 
 namespace ViewModel
 {
