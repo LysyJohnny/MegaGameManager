8fa5d2de3aac75f36234c6c5a42030cdedf74d67
Daria <lysy_johnny@bk.ru>
2017-04-16 00:12:58 +0700

    Iwanttoday

--- ManagerView/ManagerView/RegistartionWindow.xaml.cs
+++ ManagerView/ManagerView/RegistartionWindow.xaml.cs
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using ViewModel;
 
 namespace ManagerView
 {
@@ -22,6 +23,7 @@ namespace ManagerView
         public RegistartionWindow()
         {
             InitializeComponent();
+            DataContext = new Class1();
         }
 
         private void StartGame_Click(object sender, RoutedEventArgs e)
--- ManagerView/ViewModel/Class1.cs
+++ ManagerView/ViewModel/Class1.cs
@@ -8,10 +8,74 @@ using System.Collections.ObjectModel;
 using System.ComponentModel;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
+using System.Runtime.CompilerServices;
 
 namespace ViewModel
 {
-    public class Class1 
+    public class Class1 : INotifyPropertyChanged
     {
+
+        private ObservableCollection<Player> _player;
+        private Player _selectedPlayer;
+        //private string NamePlayer;
+      
+            public Player SelectedPlayer
+        {
+            get { return _selectedPlayer; }
+            set { _selectedPlayer = value; }
+        }
+        public ObservableCollection<Player> Players
+        {
+            get { return _player; }
+            set
+            {
+                _player = value;
+            }
+        }
+
+
+
+        public Player SelectedStudent
+        {
+            get { return _selectedPlayer; }
+            set { _selectedPlayer = value; }
+        }
+
+       public string NamePlayer { get; set; }
+       /* {
+            get { return NamePlayer; }
+            set { NamePlayer = value; }
+        }*/
+
+        public RelayCommand AddNamePlayerCommand { get; set; }
+        public Class1()
+        {
+            Players = new ObservableCollection<Player>();
+            AddNamePlayerCommand = new RelayCommand(AddPlayerName);
+            
+        }
+
+        public void AddPlayerName()
+        {
+            if (NamePlayer != null)
+            {
+                Player player = new Player
+                {
+                    Name = NamePlayer,
+                    
+
+                };
+                Players.Add(player);
+
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    
     }
 }
